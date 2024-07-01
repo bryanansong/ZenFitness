a70a5f8cdeb325113a3614c9ec5057fb83008e79
Bryan Ansong <bryanansong2003@gmail.com>
2024-07-01 15:36:19 -0700

    defined types

---
+++ frontend/types.d.ts
@@ -0,0 +1,92 @@
+export {};
+
+declare global {
+  type Exercise = {
+    id: number;
+    name: string;
+    videoUrl: string;
+    workoutTemplateExercises: any;
+    createdAt: Date;
+  };
+
+  type User = {
+    id: number;
+    username: string;
+    email: string;
+    password: string;
+    workoutTemplates: WorkoutTemplate[];
+    workoutSessions: WorkoutSession[];
+    following: Follow[];
+    followers: Follow[];
+    votes: Vote[];
+  };
+
+  type Follow = {
+    id: number;
+    followerId: number;
+    followingId: number;
+    createdAt: Date;
+    follower: User;
+    following: User;
+  };
+
+  type WorkoutTemplate = {
+    id: number;
+    userId: number;
+    user: User;
+    name: string;
+    isPublic: boolean;
+    exercises: WorkoutTemplateExercise[];
+    workoutSessions: WorkoutSession[];
+    copyCount: number;
+    votes: Vote[];
+    createdAt: Date;
+  };
+
+  type WorkoutTemplateExercise = {
+    workoutTemplateId: number;
+    exerciseId: number;
+    workoutTemplate: WorkoutTemplate;
+    exercise: Exercise;
+  };
+
+  type Exercise = {
+    id: number;
+    name: string;
+    videoUrl: string | null;
+    workoutTemplateExercises: WorkoutTemplateExercise[];
+    setRecords: SetRecord[];
+  };
+
+  type WorkoutSession = {
+    id: number;
+    userId: number;
+    workoutTemplateId: number;
+    date: Date;
+    duration: number;
+    completionStatus: "COMPLETED" | "PARTIAL";
+    user: User;
+    workoutTemplate: WorkoutTemplate;
+    setRecords: SetRecord[];
+    createdAt: Date;
+  };
+
+  type SetRecord = {
+    id: number;
+    workoutSessionId: number;
+    exerciseId: number;
+    workoutSession: WorkoutSession;
+    exercise: Exercise;
+    reps: number;
+    weight: number;
+  };
+
+  type Vote = {
+    id: number;
+    userId: number;
+    workoutTemplateId: number;
+    voteType: "UPVOTE" | "DOWNVOTE";
+    user: User;
+    workoutTemplate: WorkoutTemplate;
+  };
+}
