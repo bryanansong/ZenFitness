7f3c07ae4ebe719e65095ef75e9943a449ae942d
Bryan Ansong <bryanansong2003@gmail.com>
2024-07-01 23:47:36 -0700

    created one instance of prisma client

--- backend/utils/helpers.js
+++ backend/utils/helpers.js
@@ -1,4 +1,7 @@
 import bcrypt from "bcrypt";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 const hashPassword = async (password) => {
   return await bcrypt.hash(password, 10);
@@ -8,4 +11,4 @@ const comparePassword = async (inputPassword, hashedPassword) => {
   return await bcrypt.compare(inputPassword, hashedPassword);
 };
 
-export { hashPassword, comparePassword };
+export { prisma, hashPassword, comparePassword };
