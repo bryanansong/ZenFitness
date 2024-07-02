dd59333dd495bb5dfe7d6bf77556d73d719908c4
Bryan Ansong <bryanansong2003@gmail.com>
2024-07-02 16:21:19 -0700

    removed old navigation components

--- frontend/src/navigation/Authentication/Authentication.tsx
+++
@@ -1,7 +0,0 @@
-import styles from "./Authentication.module.css";
-
-const Authentication = () => {
-  return <div>Authentication</div>;
-};
-
-export default Authentication;
--- frontend/src/navigation/VerifiedNavigation/VerifiedNavigation.tsx
+++
@@ -1,7 +0,0 @@
-import styles from "./VerifiedNavigation.module.css";
-
-const VerifiedNavigation = () => {
-  return <div>VerifiedNavigation</div>;
-};
-
-export default VerifiedNavigation;
