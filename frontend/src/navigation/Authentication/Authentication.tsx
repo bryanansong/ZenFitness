004af0b5113550fab2195dd4ed449ebb69d5b87a
Bryan Ansong <bryanansong2003@gmail.com>
2024-07-02 15:44:03 -0700

    instantiated authentication and verified navigation routes

---
+++ frontend/src/navigation/Authentication/Authentication.tsx
@@ -0,0 +1,7 @@
+import styles from "./Authentication.module.css";
+
+const Authentication = () => {
+  return <div>Authentication</div>;
+};
+
+export default Authentication;
---
+++ frontend/src/navigation/VerifiedNavigation/VerifiedNavigation.tsx
@@ -0,0 +1,7 @@
+import styles from "./VerifiedNavigation.module.css";
+
+const VerifiedNavigation = () => {
+  return <div>VerifiedNavigation</div>;
+};
+
+export default VerifiedNavigation;
