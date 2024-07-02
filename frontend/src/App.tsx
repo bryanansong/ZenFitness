463eefac3e6ff05be46f074b567fd118a55a78fb
Bryan Ansong <bryanansong2003@gmail.com>
2024-07-02 16:25:20 -0700

    created a route for all screens

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,48 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SideBar from "./components/SideBar/SideBar";
+import Feed from "./screens/Feed/Feed";
+import History from "./screens/History/History";
+import Dashboard from "./screens/Dashboard/Dashboard";
+import Login from "./screens/auth/Login/Login";
+import SignUp from "./screens/auth/SignUp/SignUp";
 
 function App() {
 
   return (
-    <>
-      <h1>Boiler Plate</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="*" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route
+          path="/dashboard"
+          element={
+            <div className="app">
+              <SideBar />
+              <Dashboard />
+            </div>
+          }
+        />
+        <Route
+          path="/feed"
+          element={
+            <div className="app">
+              <SideBar />
+              <Feed />
+            </div>
+          }
+        />
+        <Route
+          path="/history"
+          element={
+            <div className="app">
+              <SideBar />
+              <History />
+            </div>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
