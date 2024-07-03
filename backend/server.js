895e0103114784f7fe500be606278a4f5e90f13e
Bryan Ansong <bryanansong2003@gmail.com>
2024-07-03 01:32:44 -0700

    created authentication middleware and handled cors issues

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,21 @@
 import express, { json } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
 import authRoutes from "./routes/authRoutes.js";
+import { authenticateToken } from './middleware/authMiddleware.js';
 
+dotenv.config();
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+app.use(cors());
 app.use(json());
 
 app.use("/auth", authRoutes);
+
+app.use("", authenticateToken);
+
+// Protected Routes
 
 // START SERVER
 app.listen(PORT, () => {
