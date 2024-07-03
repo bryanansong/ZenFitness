1353052471466c1e0bb291b08ecac81223742445
Bryan Ansong <bryanansong2003@gmail.com>
2024-07-03 01:31:29 -0700

    handled login action

--- frontend/src/screens/auth/Login/Login.tsx
+++ frontend/src/screens/auth/Login/Login.tsx
@@ -1,27 +1,73 @@
 import styles from "./Login.module.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      const response = await fetch(
+        `${import.meta.env.VITE_BACKEND_URL}/auth/login`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ username, password }),
+        }
+      );
+
+      if (response.ok) {
+        const { token } = await response.json();
+        localStorage.setItem("token", token);
+        navigate("/dashboard");
+      } else {
+        // Handle error
+        console.error("Login failed");
+      }
+    } catch (error) {
+      console.error("Error during login:", error);
+    }
+  };
+
   return (
     <div className={styles.loginContainer}>
       <div className={styles.loginContent}>
         <h2 className={styles.title}>Login</h2>
-        <form className={styles.form}>
+        <form className={styles.form} onSubmit={handleSubmit}>
           <div className={styles.inputGroup}>
             <label htmlFor="username" className={styles.label}>
               Username
             </label>
-            <input type="text" id="username" className={styles.input} />
+            <input
+              type="text"
+              id="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value.trim())}
+              className={styles.input}
+              required
+            />
           </div>
           <div className={styles.inputGroup}>
             <label htmlFor="password" className={styles.label}>
               Password
             </label>
-            <input type="password" id="password" className={styles.input} />
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value.trim())}
+              className={styles.input}
+              required
+            />
           </div>
           <div className={styles.buttonGroup}>
             <Link to={"/signup"} className={styles.signUpButton}>
-                Sign Up
+              Sign Up
             </Link>
             <button type="submit" className={styles.continueButton}>
               Continue
