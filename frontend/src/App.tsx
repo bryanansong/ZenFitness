a10cc99020f190230941c27fc60e269f66c11bcd
Bryan Ansong <bryanansong2003@gmail.com>
2024-07-02 17:30:56 -0700

    fixed navigation path to login screen

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -12,7 +12,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="*" element={<Login />} />
+        <Route path="/" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
         <Route
           path="/dashboard"
