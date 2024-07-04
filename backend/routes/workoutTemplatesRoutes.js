bae99f65b56bb1359ae6deb571036e201ba3f662
Bryan Ansong <bryanansong2003@gmail.com>
2024-07-04 14:11:57 -0700

    added route for api path to retrieve workout information

--- backend/routes/workoutTemplatesRoutes.js
+++ backend/routes/workoutTemplatesRoutes.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
-import { createWorkoutTemplate, getFeed, getWorkoutTemplates } from "../controllers/workoutTemplatesController.js";
+import { createWorkoutTemplate, getFeed, getWorkoutTemplateInfo, getWorkoutTemplates } from "../controllers/workoutTemplatesController.js";
 
 const router = Router();
 
 // Creating a template
 router.post("/", createWorkoutTemplate);
+
 router.get("/feed", getFeed);
+router.get("/:templateId", getWorkoutTemplateInfo);
 router.get("/", getWorkoutTemplates);
 
 export { router };
