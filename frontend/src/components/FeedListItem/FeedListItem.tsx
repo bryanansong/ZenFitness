3e87aaff773adc7fbf166d6a13eb38b24f191cb0
Bryan Ansong <bryanansong2003@gmail.com>
2024-07-08 16:55:16 -0700

    created function to handle a user vote action

--- frontend/src/components/FeedListItem/FeedListItem.tsx
+++ frontend/src/components/FeedListItem/FeedListItem.tsx
@@ -1,10 +1,63 @@
+import { useEffect, useState } from "react";
 import styles from "./FeedListItem.module.css";
 
 type FeedListItemProps = {
-  post: WorkoutTemplate,
+  post: WorkoutTemplate;
 };
 
 const FeedListItem: React.FC<FeedListItemProps> = ({ post }) => {
+  const [upvote, setUpvote] = useState<boolean | null>(null);
+  const [netVotes, setNetVotes] = useState<number>(0);
+
+  const calculateNetVotes = () => {
+    if (!post || !post.votes) {
+      setNetVotes(0);
+      return;
+    }
+
+    const upvotes = post.votes.filter(
+      (vote) => vote.voteType === "UPVOTE"
+    ).length;
+    const downvotes = post.votes.filter(
+      (vote) => vote.voteType === "DOWNVOTE"
+    ).length;
+    const netVotes = upvotes - downvotes;
+
+    setNetVotes(netVotes);
+  };
+
+  const handleVote = async (voteType: "UPVOTE" | "DOWNVOTE") => {
+    try {
+      const response = await fetch(
+        `${import.meta.env.VITE_BACKEND_URL}/workout-templates/${post.id}/vote`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+          body: JSON.stringify({ voteType }),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Failed to update vote");
+      }
+
+      const updatedPost = await response.json();
+      post.votes = updatedPost.votes;
+      calculateNetVotes();
+
+      setUpvote(voteType === "UPVOTE");
+    } catch (error) {
+      console.error("Error updating vote:", error);
+    }
+  };
+
+  useEffect(() => {
+    calculateNetVotes();
+  }, [post]);
+
   return (
     <div className={styles.container}>
       <div className={styles.overview}>
