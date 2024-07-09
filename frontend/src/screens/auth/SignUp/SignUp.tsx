e3aa5ceae354cf4f8d1d30b342ba4c887643dc9a
Bryan Ansong <bryanansong2003@gmail.com>
2024-07-08 22:13:39 -0700

    extracted user info from backend on signup

--- frontend/src/screens/auth/SignUp/SignUp.tsx
+++ frontend/src/screens/auth/SignUp/SignUp.tsx
@@ -23,8 +23,10 @@ const SignUp = () => {
       );
 
       if (response.ok) {
-        const { token } = await response.json();
+        const { token, user } = await response.json();
         localStorage.setItem("token", token);
+        localStorage.setItem("username", user.username);
+        localStorage.setItem("userId", user.userId);
         navigate("/dashboard");
       } else {
         // Handle error
