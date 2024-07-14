ef3d42c0b1b906d05babb92d0d339012edc6155f
Bryan Ansong <bryanansong2003@gmail.com>
2024-07-13 20:22:51 -0700

    added pagination logic to the frontend

--- frontend/src/screens/Feed/Feed.tsx
+++ frontend/src/screens/Feed/Feed.tsx
@@ -2,14 +2,20 @@ import { useEffect, useState } from "react";
 import Header from "../../components/Header/Header";
 import styles from "./Feed.module.css";
 import FeedListItem from "../../components/FeedListItem/FeedListItem";
+import arrowRight from "../../assets/arrow-circle-right.svg";
+import arrowLeft from "../../assets/arrow-circle-left.svg";
 
 const Feed = () => {
-  const [postList, setPostList] = useState([]);
+  const [postList, setPostList] = useState<WorkoutTemplate[]>([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
 
-  const fetchTemplates = async () => {
+  const fetchTemplates = async (page = 1) => {
     try {
       const response = await fetch(
-        `${import.meta.env.VITE_BACKEND_URL}/workout-templates/feed`,
+        `${
+          import.meta.env.VITE_BACKEND_URL
+        }/workout-templates/feed?page=${page}&limit=2`,
         {
           method: "GET",
           headers: {
@@ -22,7 +28,9 @@ const Feed = () => {
         throw new Error("Failed to fetch feed");
       }
       const data = await response.json();
-      setPostList(data);
+      setPostList(data.recommendations);
+      setCurrentPage(data.currentPage);
+      setTotalPages(data.totalPages);
     } catch (err: any) {
       console.error(err.message);
     }
@@ -31,6 +39,18 @@ const Feed = () => {
   useEffect(() => {
     fetchTemplates();
   }, []);
+
+  const handleNextPage = () => {
+    if (currentPage < totalPages) {
+      fetchTemplates(currentPage + 1);
+    }
+  };
+
+  const handlePrevPage = () => {
+    if (currentPage > 1) {
+      fetchTemplates(currentPage - 1);
+    }
+  };
 
   return (
     <div className={styles.container}>
@@ -43,6 +63,25 @@ const Feed = () => {
               <FeedListItem post={post} />
             </div>
           ))}
+        </div>
+        <div className={styles.pagination}>
+          <button
+            className={styles.pageButton}
+            onClick={handlePrevPage}
+            disabled={currentPage === 1}
+          >
+            <img src={arrowLeft} alt="Previous Page" />
+          </button>
+          <span>
+            Page {currentPage} of {totalPages}
+          </span>
+          <button
+            className={styles.pageButton}
+            onClick={handleNextPage}
+            disabled={currentPage === totalPages}
+          >
+            <img src={arrowRight} alt="Next Page" />
+          </button>
         </div>
       </div>
     </div>
