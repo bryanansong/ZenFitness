5dfa3aa8114a30a06ab816d3d5c02e82cc94fecb
Bryan Ansong <bryanansong2003@gmail.com>
2024-07-26 23:49:12 -0700

    added middle ware for uploading photos

--- backend/routes/progressPhotoRoutes.js
+++ backend/routes/progressPhotoRoutes.js
@@ -5,12 +5,17 @@ import {
   savePhotoDetails,
   deletePhoto,
 } from "../controllers/progressPhotoController.js";
+import multer from "multer";
 
 const router = Router();
 
+// photos middleware
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
 router.get("/", getProgressPhotos);
 router.get("/upload-url", getUploadUrl);
-router.post("/", savePhotoDetails);
+router.post("/", upload.single("image"), savePhotoDetails);
 router.delete("/:id", deletePhoto);
 
 export default router;
