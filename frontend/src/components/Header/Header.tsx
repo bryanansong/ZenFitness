dc72745087fed94f9a4e032678bed8e63d5847bc
Bryan Ansong <bryanansong2003@gmail.com>
2024-08-01 23:30:12 -0700

    removed seach bar

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -2,10 +2,13 @@ import styles from "./Header.module.css";
 
 const Header = () => {
   return (
-    <div className={styles.container}>
+    <>
+      <br />
+      {/* <div className={styles.container}>
       <input className={styles.searchBar} type="text" name="" id="" placeholder="Search for an exercise..."/>
-    </div>
-  )
-}
+      </div> */}
+    </>
+  );
+};
 
-export default Header
+export default Header;
